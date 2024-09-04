84320103806ee2c1aef56c225b4076f89a0d2a18
Dancan <ondiekdancan0@gmail.com>
2024-09-04 14:41:27 +0300

    Add files via upload

---
+++ exam/test.js
@@ -0,0 +1,7 @@
+let Name = getElementById(Name).value;
+let Email= getElementById(Email).value;
+let Gender = getElementById(Gender).value;
+let Phonenumber = getElementById(Phonenumber).value
+if(username === "" || password === ""){
+    alert("fill in the fields");
+}
